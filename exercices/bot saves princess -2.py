cb98a4abef6e065ee8ce5c2d0c23681a362e82ee
ccmorenom <camilo@theblackpuma.com>
2020-09-22 17:40:48 -0300

    finish bot save princess -2

--- exercices/bot saves princess -2.py
+++ exercices/bot saves princess -2.py
@@ -1,39 +1,23 @@
-
-import numpy as np
-
-def move_h(x):
-    if x<0:
-        return 'left'
-    elif x==0:
-        return 'no move'
+def nextMove(n,r,c,grid):
+    if True in [('p' in (grid[i])) for i in range(r)]:
+        bolean = [('p' in (grid[i])) for i in range(r)]
+        r_f = ([i for i in range(len(bolean)) if bolean[i]])[0]
+        c_f = [i for i in range(len(grid[r_f])) if (grid[r_f][i]=='p')][0]
+        p_p = (r_f,c_f)
     else:
-        return 'right'
-def move_v(x):
-    if x<0:
-        return 'upp'
-    elif x==0:
-        return 'no move'
+        bolean = [('p' in (grid[i])) for i in range(r,n)]
+        r_f = ([i+r for i  in range(len(bolean)) if bolean[i]])[0]        
+        c_f = [i for i in range(len(grid[r_f])) if (grid[r_f][i]=='p')][0]
+        p_p = (r_f,c_f)
+        
+    if r-p_p[0] == 0:
+        if c-p_p[1] >0 :
+            result = 'LEFT'
+        else:
+            result = 'RIGHT'
+    elif r-p_p[0] >0:
+        result = 'UP'
     else:
-        return 'down'
-
-def save_prince(H,V):
-    start=True
-    while start:
-        grid=np.zeros((V,H),dtype=str)
-        grid[grid == '0'] = '-' 
-        vm,hm = np.random.randint(0,V,size=2)
-        grid[vm,hm]='m'
-        vp,hp = np.random.randint(0,V,size=2)
-        grid[vp,hp]='p'
-        rng_win = [x*2 for x in range(-1,2)] 
-        win = [(vp,hp+j) for j in  rng_win if j != 0] + [(vp+i,hp) for i in rng_win if i != 0]
-        start = (vp,hp) in win
-    move=[(i[0]-vm,i[1]-hm) for i in win]
-    count_move=[(abs(move[i][0]) + abs(move[i][1])) for i in range(len(move))]
-    desi=[i == min(count_move) for i in count_move]
-    steps=[move[i] for i in range(len(move)) if desi[i]]
-    print(np.array(grid)) 
-    for step in steps:
-        print(f" {abs(step[0])} steps {move_v(step[0])} and  {abs(step[1])} steps {move_h(step[1]) }")
-save_prince(10,10)
-
+        result = 'DOWN'
+    
+    return result
